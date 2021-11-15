d78e4c4ce18a9ed8351c24ad3a4feee94407654b
Raul <link_2642@hotmail.com>
2021-11-15 12:39:03 -0600

    añadido centro y zoom mapa  fullscreen

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from '../../../../environments/environment';
- 
 
 @Component({
   selector: 'app-full-screen',
@@ -22,11 +20,13 @@ export class FullScreenComponent implements OnInit {
 
   ngOnInit(): void {
 
-    (mapboxgl as any).accessToken = environment.mapboxToken;
+    
 
     var map = new mapboxgl.Map({
     container: 'mapa',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [  -98.21521330594446, 19.022949938201375 ],
+    zoom: 16
 });
   }
 
