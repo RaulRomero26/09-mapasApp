b7171c76758f2c5ccc327a37e0e28e307e8f0e99
Raul Romero <link_2642@hotmail.com>
2021-11-18 16:01:42 -0600

    barra de zoom actualiza

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -41,16 +41,21 @@ export class ZoomRangeComponent implements AfterViewInit {
       center: [  -98.21521330594446, 19.022949938201375 ],
       zoom: this.zoomLevel,
       });
-  }
+
+      this.mapa.on('zoom', (ev) => {
+        this.zoomLevel = this.mapa.getZoom();
+      })
+    }
+    
 
   zoomOut(){
     this.mapa.zoomOut();
-    this.zoomLevel = this.mapa.getZoom();
+    
   }
 
   zoomIn(){
     this.mapa.zoomIn();
-    this.zoomLevel = this.mapa.getZoom();
+  
   }
 
 }
